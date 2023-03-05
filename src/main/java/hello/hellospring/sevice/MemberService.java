f1a936aa19141a5fe7eab0fae55251dcc9b15b51
dkagh012 <qowjdxo00@gmail.com>
2023-03-05 21:56:46 +0900

    회원 서비스 작성

---
+++ src/main/java/hello/hellospring/sevice/MemberService.java
@@ -0,0 +1,35 @@
+package hello.hellospring.sevice;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository= new MemoryMemberRepository();
+
+    /*회원가입*/
+    public Long join(Member member){
+//        같은 이름이 있는 중복 회원은 안된다
+        validateDuplicateMember(member); /*중복회원검증*/
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent(m -> {
+            /*reuslt 가 null 아닌 값일 때 동작한다. 이유는 optional이라서 null*/
+            throw new IllegalStateException("이미 존재 합니다.");
+        });
+    }
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
